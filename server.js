26f2cb34512289b609e3a3fa6df6a79659fa39e3
Steven Miyauchi <swm9220@gmail.com>
2019-12-11 10:28:41 -0800

    lab-00b-resub

--- server.js
+++ server.js
@@ -13,41 +13,56 @@ app.use(cors());
 app.get('/location', (req, res) => {
     let city = req.query.data;
     let URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-    // let newLocation = searchLatToLong(city);
-    let testLocation = {
-        "search_query": "seattle",
-        "formatted_query": "Seattle, WA, USA",
-        "latitude": "47.606210",
-        "longitude": "-122.332071"
-    }
-    res.send(testLocation);
 
+    superagent.get(URL).then(cityData => {
+        // let testLocation = new Location(city, cityData.body.results[0]);
+        // let newLocation = {
+        //     "search_query": city,
+        //     "formatted_query": cityData.body.results[0].formatted_address,
+        //     "latitude": cityData.body.results[0].geometry.location.lat,
+        //     "longitude": cityData.body.results[0].geometry.location.lng,
+        // }
+        res.send(new Location(city, cityData.body.results[0]));
+    })
 });
 
-// https://maps.googleapis.com/maps/api/geocode/json?address=Sydney&key=AIzaSyBeEMZYda9yK4YNrrnwLUb_e9mERJYYER8
+function Location(city, cityData){
 
-// app.get('/weather', (req, res) => {
-//     let weather = req.query.data2;
-
-//     let newWeather = searchDaily(weather);
-
-//      res.send(newWeather);
-
-function searchLatToLong(city){
-    const geoData = require('./data/geo.json');
-
-    const geoDataResults = geoData.results[0];
-
-    const newLocation = new Location(city, geoDataResults);
-    // const newLocation = {
-    //     "search_query": city,
-    //     "formatted_query": geoDataResults.formatted_address,
-    //     "latitude": geoDataResults.geometry.location.lat,
-    //     "longitude": geoDataResults.geometry.location.lng
-    // }
-
-    return newLocation;
+    this.search_query = city;
+    this.formatted_query = cityData.formatted_address;
+    this.latitude = cityData.geometry.location.lat;
+    this.longitude = cityData.geometry.location.lng;
 }
+
+app.get('/weather', (req, res) => {
+    let weather = req.query.data2;
+    let URL2 = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data2}&key=${process.env.GEOCODE_API_KEY}`;
+
+    superagent.get(URL2).then(weatherData => {
+        res.send(new Weather(weather, weatherData.body));
+    })
+});
+
+function Weather(weather, weatherData){
+    this.timezone = weather;
+    this.daily = weatherData.daily.data[0].summary;
+}
+
+// function searchLatToLong(city){
+//     const geoData = require('./data/geo.json');
+
+//     const geoDataResults = geoData.results[0];
+
+//     const newLocation = new Location(city, geoDataResults);
+//     const newLocation = {
+//         "search_query": city,
+//         "formatted_query": geoDataResults.formatted_address,
+//         "latitude": geoDataResults.geometry.location.lat,
+//         "longitude": geoDataResults.geometry.location.lng
+//     }
+
+//     return newLocation;
+// }
 
 // function searchWeather(weather){
 //     const darkSkyData = require('./data/geo.json');
@@ -62,13 +77,6 @@ function searchLatToLong(city){
 // function Weather(weather, data2){
 //     //will add later today
 // }
-
-function Location(city, data){
-    this.search_query = city;
-    this.formatted_query + data.formatted_address;
-    this.latitude = data.location.lat;
-    this.longitude = data.geometry.location.lng;
-}
 
 app.get('*', (req, res) => {
     res.status(404).send('Page not found');
